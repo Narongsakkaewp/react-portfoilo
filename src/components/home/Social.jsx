c6e143f4359ba7bf9538b4113d787739b77fe3b7
Narongsakkaewp <152575175+Narongsakkaewp@users.noreply.github.com>
2025-03-07 19:02:57 +0700

    สร้าง header กับ home และก็ import พวก assets ที่จำเป็น
    home เหลือทำ css ให้เสร็จ วันนี้ง่วงแล้ว555555

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="#https://www.instagram.com/" className="home__socail-icon" target="_blank">
+            <i class="uil uil-instagram"></i>
+        </a>
+
+        <a href="#https://www.instagram.com/" className="home__socail-icon" target="_blank">
+            <i class="uil uil-github"></i>
+        </a>
+
+        <a href="#https://www.instagram.com/" className="home__socail-icon" target="_blank">
+            <i class="uil uil-facebook"></i>
+        </a>
+
+        <a href="#https://www.instagram.com/" className="home__socail-icon" target="_blank">
+            <i class="uil uil-google-drive-alt"></i>
+        </a>
+
+        <a href="#https://www.instagram.com/" className="home__socail-icon" target="_blank">
+            <i class="uil uil-music"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
